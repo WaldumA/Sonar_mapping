a6f91e39961df2af5ce516dbf9ebe3c479683248
WaldumA <ambjornwaldum@gmail.com>
2020-02-11 11:33:00 +0100

    Working on sonar camera calibration

--- src/calibration_misc.py
+++ src/calibration_misc.py
@@ -6,8 +6,6 @@ from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-
-
 # Takes inn two numpy arrays y and x, and finds the best line fit for all points, writes intersection and slope to calibration.txt
 def fit_line_least_squares(self,y,x):
         # x and y is numpy arrays
@@ -32,7 +30,100 @@ def convert_to_openCV(image_msg):
         image = bridge.imgmsg_to_cv2(image_msg, "bgr8")
         cv2.imshow('Display',image)
         cv2.waitKey(1)
-        return image
     except:
         return []
 
+# Switcher for handling user input
+def input_switcher(user_input):
+    Switcher = {
+        0: 'Save',
+        1: 'Calibrate',
+    }
+    task = Switcher.get(user_input,'skip')
+    if task == 'skip':
+        return 'Skip'
+    elif task == 'Save':
+        return 'Save'
+    elif task == 'Calibrate':
+        return 'Calibrate'
+
+# Return bearing, range and pixel width of current msg
+def Save_current_frame(sonar_data, image_data):
+    # Extracting necesarry data from msgs
+    angle_increment = sonar_data.angle_increment
+    current_angle = sonar_data.angle_min
+    scan_ranges = sonar_data.ranges
+    bridge = CvBridge()
+    image = bridge.imgmsg_to_cv2(image_data, "bgr8")
+    # Appending all promising scans to a list as tuples [range_of_scan, bearing_from_manta]
+    possible_scans = get_promising_sonar_scans(angle_increment,current_angle,scan_ranges)
+
+    # Calculating average range and bearing
+    sonar_range, sonar_bearing = calculate_best_sonar_data(possible_scans)
+
+    # Finds current pixel width of pole
+    pixel_width = get_pixel_width(image)
+
+    return pixel_width, sonar_bearing, sonar_range 
+
+
+
+
+# Calibrate
+def calibrate_lines():
+    pass
+
+# Writing fitted variables to a .txt file
+def write_to_file():
+    pass
+
+# Returns all depth scans betlow a certain threshold with bearing
+def get_promising_sonar_scans(angle_increment,current_angle,scan_ranges):
+    possible_scans = []
+    counter = 0
+    for depth in scan_ranges:
+        if depth < 10:
+            possible_scans.append(tuple([depth,current_angle]))
+            counter += 1
+        current_angle += angle_increment
+    return possible_scans
+
+# Filters a colored pole and detects its bounding box
+def get_pixel_width(image):
+    # Transforms the image to HSV which is a color space easier to filter 
+    image_hsv=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # In the HSV colorspace RED is represented both from 170-180 and 0-10
+    # 0-10
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+    mask0 = cv2.inRange(image_hsv, lower_red, upper_red)
+    # 170-180
+    lower_red = np.array([170,50,50])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(image_hsv, lower_red, upper_red)
+
+    # Combining upper and lower values
+    mask = mask0 + mask1
+    
+    # Finds contours
+    _,contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    area = 0
+    bbox = -1  
+    pixel_width = -1
+    for contour in contours:
+        if cv2.contourArea(contour) > area:
+            area = cv2.contourArea(contour)
+            bbox = cv2.boundingRect(contour)
+       
+    if bbox != -1:
+        pixel_width = bbox[0] + int((bbox[0]+bbox[2])/2)
+        ''' 
+        # Visualising bounding box for troubleshooting purposes
+        cv2.rectangle(image,(bbox[0],bbox[1]),(bbox[0]+bbox[2],bbox[1]+bbox[3]),(0,255,0), 3)  
+        cv2.imshow('Display',image)
+        cv2.waitKey()
+        '''
+    return pixel_width
+    
+def cal
--- src/camera_sonar_calibration.py
+++ src/camera_sonar_calibration.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import rospy
 import numpy as np
 from sensor_msgs.msg import LaserScan
@@ -11,13 +12,32 @@ import cv2
 class MANUAL_CALIBRATION:
 
     def calibrationCallback(self,data):
+        # Get input from user
+        user_input = -1
+        user_input = input()
+        output = calibration_misc.input_switcher(user_input)
+
+        # A switch case for what to do with user input
+        if output == 'Save':
+            current_width, current_bearing, current_range = calibration_misc.Save_current_frame(self.sonar_data, self.image_data)
+        elif output == 'Calibrate':
+            print("Mello")
+
+
+
+
+
+
+
+
+
+
+
+   
+    def visualisingCallback(self,data):
         # Visualising both sonar and camera feed
-        image = calibration_misc.convert_to_openCV(self.image_data)
+        calibration_misc.convert_to_openCV(self.image_data)
         mapping_functions.imageCurrentScan(self.sonar_data,WALL_WIDTH=3,SCALE=100)
-        # Saving point
-        calibration_misc.save_point
-
-
 
     def imageCallback(self,data):
         self.image_data = data
@@ -29,12 +49,15 @@ class MANUAL_CALIBRATION:
         # Necesarry variables
         self.sonar_data = LaserScan()
         self.image_data = Image()
-        self.y = []
-        self.x = []
+        self.image = 0
+        self.pixel_width = []
+        self.sonar_bearing = []
+        self.sonar_range = []
         
         # Necesarry callback functions
         rospy.Subscriber('manta/sonar',LaserScan,self.sonarCallback)
         rospy.Subscriber('manta/manta/camerafront/camera_image',Image,self.imageCallback)
+        #rospy.Timer(rospy.Duration(1.0/10.0),self.visualisingCallback)
         rospy.Timer(rospy.Duration(1.0/10.0),self.calibrationCallback)
 
 
